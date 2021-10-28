f59d6491c57aab55598200ad417e7a809d7ddf58
pekeso <joembiye@gmail.com>
2021-10-28 19:12:51 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe.dart';
+
+void main() {
+  runApp(RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        primary: Colors.grey,
+        secondary: Colors.black,
+      )),
+      home: MyHomePage(title: 'Recipe Calculator0'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(Recipe.samples[index].label);
+          },
+        ),
+      ),
+    );
+  }
+}
